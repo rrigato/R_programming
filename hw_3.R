cbd4b48feba06c539431315e97e3910c0005cb04
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-04 20:53:25 -0800

    Create hw_3.R

---
+++ hw_3.R
@@ -0,0 +1,12 @@
+name = "Ryan Rigato"
+ticket.line <- function (n, sim.length)
+{
+	#input validation: checks if n is less than 0
+	if (n <=0)
+	{
+		print("Error: n must be a positive 0");
+		return();
+	}
+}
+
+ticket.line(5,10)

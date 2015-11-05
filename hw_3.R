5b0e2ff8a90177e54bacb1603e0ed5caf0269847
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-04 21:13:49 -0800

    Update hw_3.R

--- hw_3.R
+++ hw_3.R
@@ -10,3 +10,44 @@ ticket.line <- function (n, sim.length)
 }
 
 ticket.line(5,10)
+
+
+
+
+n = 5
+
+line = numeric (n*2)
+ran_nums = sample(1:(n*2), 10, replace = FALSE)
+ran_nums 
+
+line[ran_nums[1:n]] = 5
+line[ran_nums[(n+1):(2*n)]] = 10
+line
+i = 1
+while (i <= (2*n) )
+{
+change = 0
+times_closed = 0
+if (line[i] == 10)
+{
+	if (change == 0)
+	{
+		i = 2*n 
+		times_closed = times_closed + 1
+	}
+	else
+	{
+		change = change - 1
+	}
+}
+else
+{
+	change = change + 1
+	
+}
+
+i = i + 1
+
+}
+
+

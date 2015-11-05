f1136aa55c8b5ebd398ef56d5cc47ee769795d26
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-04 21:26:17 -0800

    Update hw_3.R

--- hw_3.R
+++ hw_3.R
@@ -15,39 +15,46 @@ ticket.line(5,10)
 
 
 n = 5
-
-line = numeric (n*2)
-ran_nums = sample(1:(n*2), 10, replace = FALSE)
-ran_nums 
-
-line[ran_nums[1:n]] = 5
-line[ran_nums[(n+1):(2*n)]] = 10
-line
-i = 1
-while (i <= (2*n) )
-{
-change = 0
+sim_counter = 1
+sim.length = 1000000
 times_closed = 0
-if (line[i] == 10)
-{
-	if (change == 0)
-	{
-		i = 2*n 
-		times_closed = times_closed + 1
-	}
-	else
-	{
-		change = change - 1
-	}
-}
-else
-{
-	change = change + 1
+while (sim_counter <= sim.length)
+{#outside loop start
+	line = numeric (n*2)
+	ran_nums = sample(1:(n*2), 10, replace = FALSE)
+
+	line[ran_nums[1:n]] = 5
+	line[ran_nums[(n+1):(2*n)]] = 10
+	i = 1
+	change = 0
+
+	while (i <= (2*n) )
+	{#inside loop start
+
+		if (line[i] == 10)
+		{
+			if (change == 0)
+			{
+				i = 2*n 
+				times_closed = times_closed + 1
+			}
+			else
+			{
+				change = change - 1
+			}
+		}
+		else
+		{
+			change = change + 1
 	
-}
+		}
 
-i = i + 1
+		i = i + 1
 
-}
+	} #inside loop close| each customer
+	
+	sim_counter = sim_counter + 1
 
+}#outside loop close| each simulation
 
+times_closed

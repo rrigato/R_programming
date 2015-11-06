9d95610ec6d579bb546e9cafbe756c7203c8aac9
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-05 17:49:43 -0800

    Update hw_3.R

--- hw_3.R
+++ hw_3.R
@@ -7,15 +7,6 @@ ticket.line <- function (n, sim.length)
 		print("Error: n must be a positive 0");
 		return();
 	}
-}
-
-ticket.line(5,10)
-
-
-
-sim.length = 100000
-n = 19
-
 
 #counter for how many loop iterations
 sim_counter = 1
@@ -38,36 +29,56 @@ while (sim_counter <= sim.length)
 	#counter for inner while will stop when all 2*n customers have been served
 	#or when they are out of change
 	i = 1
+	#counts how much change they have
 	change = 0
-
+	
+	#will stop iterating when all 2*n customers were served
+	#or they run out of change
 	while (i <= (2*n) )
 	{#inside loop start
 
+		#If the customer has 10 dollars, you have to give them change
 		if (line[i] == 10)
 		{
+			#if you have no change you have to shut down the both
+			#this iteration of the outer while loop will result in closing down
 			if (change == 0)
 			{
+				#will bring us out of the loop
 				i = 2*n 
 				times_closed = times_closed + 1
 			}
 			else
-			{
+			{#otherwise give them change and decrement change counter
 				change = change - 1
 			}
 		}
 		else
-		{
+		{ #increment change counter cause the person has a five dollar bill
 			change = change + 1
 	
 		}
-
+		#increment inside loop counter
 		i = i + 1
 
 	} #inside loop close| each customer
 	
+	#increment outside loop counter
 	sim_counter = sim_counter + 1
 
 }#outside loop close| each simulation
+
+return(times_closed/sim.length)
+}
+
+ticket.line(5,10000)
+
+
+
+sim.length = 100000
+n = 19
+
+
+
 times_closed
 times_closed/sim.length
-

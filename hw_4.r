6484d97e2b63348a0bf33c883457442d62fbd30a
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-13 17:40:07 -0800

    Create hw_4.r

---
+++ hw_4.r
@@ -0,0 +1,14 @@
+y_count =1;
+temp_count = 1; 
+convolution_counter = 1;
+result = numeric(max(x,y) + 1)
+while ((y_count <= length(y)) || (temp_count <= length(temp)) )
+{
+	if(y_count > length(y))
+		y_count = length(y)
+	result[i]
+	y_count = y_count + 1
+	temp_count = temp_count + 1
+} 
+print(y_count)
+print(temp_count)

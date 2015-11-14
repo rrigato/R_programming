a3cc84c07b2f32ff906629697acf239e86226028
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-13 22:08:21 -0800

    Update hw_4.r

--- hw_4.r
+++ hw_4.r
@@ -1,8 +1,12 @@
+temp = rev(x); temp
+
+x= c(1,2,4)
+y= c(1,3)
 y_count =1;
 temp_count = 1; 
 convolution_counter = 1;
 result = numeric(max(x,y) + 1)
-while ((y_count <= length(y)) || (temp_count <= length(temp)) )
+while (convolution_counter <= (max(x,y) + 1) )
 {
 	if(y_count > length(y))
 		y_count = length(y)
@@ -12,3 +16,37 @@ while ((y_count <= length(y)) || (temp_count <= length(temp)) )
 } 
 print(y_count)
 print(temp_count)
+
+
+
+x= c(1,2,4)
+y= c(1,3)
+
+if ((length(x) <= length(y)) )
+{
+	temp1 = x;
+	temp2 = y;
+}else
+{
+	temp1 = y;
+	temp2 = x;
+}
+
+
+temp1 = rev(temp1); temp1
+
+convolution_counter = 1;
+iteration_number = 0;
+result = numeric(max(x,y) + 1)
+while (convolution_counter <= (max(x,y) + 1) )
+{
+
+	if (convolution_counter <= length(temp1))
+	{
+		result[convolution_counter] =  sum(temp2[1:convolution_counter] * temp1[length(temp1):(length(temp1)-iteration_number)])
+	}
+	convolution_counter = convolution_counter + 1
+	iteration_number = iteration_number + 1;
+} 
+
+print(result)

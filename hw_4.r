8ff3b04c60246cd0432cdc953b86c51ea92d58a1
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-13 22:28:45 -0800

    Update hw_4.r

--- hw_4.r
+++ hw_4.r
@@ -1,21 +1,4 @@
-temp = rev(x); temp
-
-x= c(1,2,4)
-y= c(1,3)
-y_count =1;
-temp_count = 1; 
-convolution_counter = 1;
-result = numeric(max(x,y) + 1)
-while (convolution_counter <= (max(x,y) + 1) )
-{
-	if(y_count > length(y))
-		y_count = length(y)
-	result[i]
-	y_count = y_count + 1
-	temp_count = temp_count + 1
-} 
-print(y_count)
-print(temp_count)
+name="Ryan Rigato"
 
 
 
@@ -37,13 +20,26 @@ temp1 = rev(temp1); temp1
 
 convolution_counter = 1;
 iteration_number = 0;
-result = numeric(max(x,y) + 1)
-while (convolution_counter <= (max(x,y) + 1) )
+result = numeric(length(temp2) + 1)
+while (convolution_counter <= (length(temp2) + 1) )
 {
 
-	if (convolution_counter <= length(temp1))
+	if (convolution_counter <= length(temp2))
 	{
-		result[convolution_counter] =  sum(temp2[1:convolution_counter] * temp1[length(temp1):(length(temp1)-iteration_number)])
+		if (convolution_counter > length(temp1))
+		{
+			result[convolution_counter] =  sum(temp2[convolution_counter:(convolution_counter + 1 - length(temp1) )]
+			 * temp1[length(temp1):1])
+
+		}
+		else
+		{
+			result[convolution_counter] =  sum(temp2[convolution_counter:1] * temp1[length(temp1):(length(temp1)- (convolution_number - 1))])
+		}	
+	else
+	{
+		result
+
 	}
 	convolution_counter = convolution_counter + 1
 	iteration_number = iteration_number + 1;

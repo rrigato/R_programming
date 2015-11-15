35baeef38d309a481907b14d3bc28dde441eb7ab
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-14 18:56:57 -0800

    Update hw_4.r

--- hw_4.r
+++ hw_4.r
@@ -5,6 +5,8 @@ name="Ryan Rigato"
 x= c(1,2,4)
 y= c(1,3)
 
+conv <-function(x,y)
+{
 if ((length(x) <= length(y)) )
 {
 	temp1 = x;
@@ -21,7 +23,8 @@ temp1 = rev(temp1); temp1
 convolution_counter = 1;
 iteration_number = 0;
 result = numeric(length(temp2) + 1)
-while (convolution_counter <= (length(temp2) + 1) )
+temp1_counter = length(temp1) -1
+while (convolution_counter <= (length(x) + length(y)  - 1) )
 {
 
 	if (convolution_counter <= length(temp2))
@@ -34,15 +37,33 @@ while (convolution_counter <= (length(temp2) + 1) )
 		}
 		else
 		{
-			result[convolution_counter] =  sum(temp2[convolution_counter:1] * temp1[length(temp1):(length(temp1)- (convolution_number - 1))])
-		}	
+			result[convolution_counter] =  sum(temp2[convolution_counter:1] * temp1[length(temp1):(length(temp1)- (convolution_counter - 1))])
+		}
+	}	
 	else
 	{
-		result
-
+		while(temp1_counter > 0)
+		{
+			result[convolution_counter] = sum(temp1[temp1_counter:1]*
+			temp2[length(temp2):(length(temp2) - (temp_count-1))])
+			temp1_counter = temp1_counter - 1
+			convolution_counter = convolution_counter + 1
+		}
 	}
 	convolution_counter = convolution_counter + 1
 	iteration_number = iteration_number + 1;
 } 
+	return(result)
+}
+x= c(0,1/6,1/6,1/6,1/6, 1/6, 1/6)
+x = c(0,1,1,1,1,1,1)
+result = conv(x,x)
+cumsum((1/216)*conv(result,x))
+
+for (i in 1:24)
+{
+	result = conv(result,x)
+}
+
 
 print(result)

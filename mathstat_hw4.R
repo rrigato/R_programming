af09aa6f2a607aa273a1f83cbe475b9f1881f372
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-17 11:17:34 -0800

    Update mathstat_hw4.R

--- mathstat_hw4.R
+++ mathstat_hw4.R
@@ -1,8 +1,3 @@
-failures = seq(0,1,by=.01)
-Probability = dbeta(failures,98,4,ncp =0)
-
-plot(failures, Probability,type = 'l', main = "Graph of Beta(4,98)")
-
 #Plot of Gamma(.315,.755)
 x=1:5
 plot(x,dgamma(x,shape =.315, scale = .755), type = 'l', main = "Plot of Gamma(.315,.755)")
@@ -10,6 +5,17 @@ plot(x,dgamma(x,shape =.315, scale = .755), type = 'l', main = "Plot of Gamma(.3
 #Plot of Gamma(100.065,.10.5)
 x = seq(0,2500, by = 10)
 plot(x,dgamma(x,shape =100.065, scale = 10.5), type = 'l', main = "Plot of Gamma(100.065,.10.5)")
+
+
+
+x =seq(0,.3, by =.01)
+#Graph of Beta(4,98)
+plot(x, dbeta(x,4,98,ncp =0),type = 'l', main = "Graph of Beta(4,98)")
+
+x =seq(0,.3, by =.01)
+#Graph of Beta(3.5,102)
+plot(x, dbeta(x,3.5,102,ncp =0),type = 'l', main = "Graph of Beta(3.5,102)")
+
 
 
 

fbeeb48c1ca428ec712d426754c07c10b4e549f9
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-25 20:47:58 -0800

    Update hw_5.R

--- hw_5.R
+++ hw_5.R
@@ -10,11 +10,13 @@ detect.misclass <- function(class.v,p)
 		print("Error: length(class.v) must equal the number of rows in p");
 		return();
 	}
+	
+	
 }
 
 
 
-z = dist(p)
+is.matrix(z) = dist(p)
 p
 class.vec
 
@@ -25,3 +27,18 @@ Far = dist(test3)
 Far
 
 dist_mat = sqrt(sum((Far[1:n,1:n] - Far[1,1])))
+z
+z[1,1:130]
+g = numeric(10); g
+temp = numeric(1)
+z = as.matrix(dist(p))
+for ( i in 1: length(p))
+{
+	for (g in 1:length(p))
+	{
+		if ((z[1,g] != 0) &&(temp > z[1,g]) )
+		{
+			temp = z[1,g
+		}
+	}
+}

0cd8deff86d21216f43b1cd4c8f76c5a0038cb17
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-29 16:20:39 -0800

    Update hw_5.R

--- hw_5.R
+++ hw_5.R
@@ -1,5 +1,5 @@
 
-name = 'Ryan Rigato'
+name = "Ryan Rigato"
 
 #####################################################################################
 #	Make sure to run install.packages('bitops') before using this function
@@ -26,251 +26,106 @@ detect.misclass <- function(class.v,p)
 	}
 	
 	
-}
 
 
 
-
-p
-class.vec
-
-test = c(1,2,3)
-test2 = c(0,1,5)
-test3 = cbind(test,test2)	
-Far = dist(test3)
-Far
-
-dist_mat = sqrt(sum((Far[1:n,1:n] - Far[1,1])))
-z
-z[1,1:130]
-g = numeric(10); g
-gnum = rep(1,10); gnum
-temp = numeric(1)
-z = as.matrix(dist(p))
-temp = numeric()
-temp
-temp = z[1,1:nrow(z)]
-temp
-for ( i in 1: length(p))
-{
-g =  z[1,1:10]
-	for (g in 1:length(p))
-	{
-		if ((z[1,g] != 0) &&(temp > z[1,g]) )
-		{
-			temp = z[1,g]
-			g
-		}
-	}
-}
-
-err.found; err.loc; new.class;
-
-return (list(err.found,err.loc,new.class));
-
-
-
-
-q
-temp = 5:1
-q = quickSort(temp,1, length(temp))
-quickSort <-function (columnVect, low, high)
-{
-
-	if (low < high)
-	{
-		part = part2 = part3 =list();
-		part = partition(columnVect, low, high)
-	browser();
-		part2 = quickSort(part[[2]], low, part[[1]]-1)
-		part3 = quickSort(part2[[2]], part2[[1]] + 1, high)
-	}
-	return(part3[[2]]);
-}
-partition <- function (columnVect, low, high)
-{
-	pivot  = columnVect[high];
-			
-	i = low
-	for (j in low:(high))
-	{
-		if(columnVect[j] <= pivot)
-		{
-			columnVect[i] = bitwXor(columnVect[i], columnVect[j])
-			columnVect[j] = bitwXor(columnVect[j], columnVect[i])
-			columnVect[i] = bitwXor(columnVect[i], columnVect[j])
-			i = i + 1;
-		}
-	}
-	columnVect[i] = bitwXor(columnVect[i], columnVect[high])
-	columnVect[high] = bitwXor(columnVect[high], columnVect[i])
-	columnVect[i] = bitwXor(columnVect[i], columnVect[high])
-
-	return(list(i, columnVect)); 
-}
-
-
-
-sortedK =1:10
-x = 5
-system.time(x*x)
-
-
-o = list(e,f)
-o[[2]]
-o[2]
-
-test = 1000:1
-row_num = 1:10
-system.time(bubbleSort(test))
-bubbleSort <- function(temp)
-{
-	row_num = 1:length(temp);
-	swap = TRUE;
-
-	while(swap)
-	{#outer while start
-	swap = FALSE
-	counter = 1;
-
-		while(counter <= length(temp) - 1)
-		{#inner while start
-			if ( temp[counter] >temp[counter + 1])
-			{
-#				holder = temp[counter]
-#				temp[counter] = temp[counter + 1]
-#				temp[counter + 1] = holder
-				holder = row_num[counter]
-				row_num[counter] =  row_num[counter + 1]
-				row_num[counter+1] = holder
-				swap = TRUE
-	
-			}
-			counter  = counter + 1
-
-		} #inner while close
 
 	
-	}#outer while close
+	this_point = 1
+	num_errors = 0
+	new.class = class.v
+	err.loc = NULL
+	err.found = FALSE
 
-	return(row_num);
+	z=as.matrix(dist(p))
+	sorted = numeric()
+
+	for (i in 1:nrow(z))
+	{
+		sorted = order(z[i,1:nrow(z)])
+		this_point = i
+		close_points = class.v[sorted[2:11]]
+		predicted_classification = round(mean(close_points))
+
+		if(predicted_classification != class.v[this_point])
+		{
+			num_errors = num_errors + 1
+			err.loc[num_errors] = this_point
+			new.class[this_point] = predicted_classification
+		}
+	}
+
+
+	if (num_errors)
+	{
+		err.found = TRUE
+	
+	}else
+	{
+		new.class = NULL
+	}
+
+	return(list(err.found, err.loc, new.class)); 
 }
 
 
-temp = 15:1
-temp; length(temp)
+
+
+order(temp)
+temp = z[5,1:132]
+temp2 = 15:1
+temp; 
 selectionSort <- function(temp)
 {
-	row_num = 1:length(temp);row_num
-	holder = 1
-	lowest = numeric(1); place = numeric(1);
-	while(holder <= 11)
-	{#outer while start
-		counter = holder
-		lowest = holder
-
-
-		while(counter <= (length(temp) - 1))
-		{#inner while start
-			if ( temp[counter] > temp[counter + 1])
-			{
-				lowest = counter	+1
-			}
-			counter  = counter + 1
-
-		} #inner while close
-
-			row_num[holder] = lowest
-			temp[lowest] = temp[holder]
-#			place = temp[lowest]
-#			temp[lowest] = temp[holder]
-#			temp[holder] = place
-		
-
-		holder = holder + 1
-
-
-	}#outer while close
-
-	return(row_num);
-
-}
-
-
-
-
-
-
-
-this_point = 1
-num_errors = 0
-new.class = p
-err.loc = null
-err.found = FALSE
-
-row_num = selectionSort
-
-close_points = p[row_num[2:11]]
-predicted_classification = round(mean(close_points))
-if(predicted_classification != p[this_point])
-{
-	num_errors = num_errors + 1
-	err.loc[num_errors] = this_point
-}
-
-
-
-
-
-
-
-if (num_errors)
-{
-	err.found = TRUE
-	
-}
-else
-{
-	new.class = null
-}
-return(list(err.found, err.loc, new.class)); 
-
-p
-class.vec
-
-dist(p)
- temp = 5:1;temp
-
-k= 5
-counter  = 1
-sorted = numeric(k)
-while (counter <=k)
-{
-	innerCounter = length(temp)
-	lowest = innerCounter
-	while (innerCounter > 1 )
+	k= 10
+	counter  = 1
+	sorted = numeric(k)
+	while (counter <=k)
 	{
-		
-		if ((temp[(innerCounter-1)] < temp[innerCounter]))
+		innerCounter = 1
+		lowest = innerCounter
+		while (innerCounter < 10 )
 		{
-			lowest = innerCounter - 1
-			
+		
+			if (temp[(innerCounter+1)] < temp[innerCounter])
+			{
+				lowest = innerCounter + 1
+				
+			}
+
+			innerCounter = innerCounter + 1
 		}
 
-		innerCounter = innerCounter - 1
+
+			sorted[counter] = lowest
+			temp[lowest] = 10^15
+
+			counter = counter + 1
 	}
-
-	print(lowest); print(temp[lowest]); print(temp[counter]);
-
-
-		sorted[counter] = temp[lowest]
-		temp[lowest] = 10^15
-
-	counter = counter + 1
+	return(sorted)
 }
-sorted
 
 
+z[2,(selectionSort(z[2,1:132]))]
+	sorted = selectionSort(z[i,1:nrow(z)])
+	sorted2 = z[i,sorted]
+	print(sorted)
+is.vector(z[5,1:nrow(z)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+row_num = selectionSort
 
 
 

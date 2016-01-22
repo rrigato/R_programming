7c749ada14feafbb9ad251acc2165b2e1b964118
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-01-21 19:56:41 -0800

    Added some work with social security in Quandl

--- mtcars.r
+++ mtcars.r
@@ -1,10 +1,14 @@
-install.packages('Quandl')
 library(Quandl)
+totalBen = as.data.frame(Quandl('SOCSEC/ALL'))
+trust = Quandl('SOCSEC/TRUST_FUND_OP')
+head(totalBen)
+plot(totalBen[101:200,1], totalBen[101:200,2], 
+xlab = "Year", ylab = "Total number of beneficifaries",
+col = "purple", main = "Total Beneficiaries 2000-2008")
 
-
-
-
-
+plot(totalBen[1:100,1], totalBen[1:100,2], 
+xlab = "Year", ylab = "Total number of beneficifaries",
+col = "pink", main = "Time series plot of average beneficiaries")
 attach(mtcars)
 data(mtcars)
 plot(wt, mpg, main="Milage vs. Car Weight", 

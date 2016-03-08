cbe8d705c1034cf5c8d6388f397ca979035b11c8
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-07 20:32:20 -0800

    Update heatmaps.R
    Added some scaling and chernoff faces

--- heatmaps.R
+++ heatmaps.R
@@ -29,3 +29,64 @@ faces(bball[,2:16])
 faces(bball[,2:16], labels=bball$Name)
 
 
+### Stars
+crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState-formatted.csv")
+stars(crime)
+row.names(crime) <- crime$state
+crime <- crime[,2:7]
+stars(crime, flip.labels=FALSE, key.loc = c(15, 1.5))
+stars(crime, flip.labels=FALSE, key.loc = c(15, 1.5), full=FALSE)
+stars(crime, flip.labels=FALSE, key.loc = c(15, 1.5), draw.segments=TRUE)
+
+### Parallel Coordinates Plot
+education <- read.csv("http://datasets.flowingdata.com/education.csv", header=TRUE)
+education[1:10,]
+
+library(lattice)
+parallel(education)
+parallel(education, horizontal.axis=FALSE)
+parallel(education[,2:7], horizontal.axis=FALSE)
+parallel(education[,2:7], horizontal.axis=FALSE, col="#000000")
+
+# Quartiles
+summary(education)
+
+# Color by reading SAT
+reading_colors <- c()
+for (i in 1:length(education$state)) {
+	
+	if (education$reading[i] > 523) {
+		col <- "#000000"	
+	} else {
+		col <- "#cccccc"	
+	}
+	reading_colors <- c(reading_colors, col)
+}
+parallelplot(education[,2:7], horizontal.axis=FALSE, col=reading_colors)
+
+# Color by dropout rate
+dropout_colors <- c()
+for (i in 1:length(education$state)) {
+	
+	if (education$dropout_rate[i] > 5.3) {
+		c <- "#000000"	
+	} else {
+		c <- "#cccccc"	
+	}
+	dropout_colors <- c(dropout_colors, c)
+}
+parallel(education[,2:7], horizontal.axis=FALSE, col=dropout_colors)
+
+
+### Multidimensional scaling
+ed.dis <- dist(education[,2:7], method="euclidean")
+ed.mds <- cmdscale(ed.dis)
+x <- ed.mds[,1]
+y <- ed.mds[,2]
+plot(x,y)
+
+plot(x, y, type="n")
+text(x, y, labels=education$state)
+text(x, y, labels=education$state, col=dropout_colors)
+text(x, y, labels=education$state, col=reading_colors)
+
